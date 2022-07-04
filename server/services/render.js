af4d485c3a377d703869544d793d46b044d6a0d7
Yatin <yatinrathod612@gmail.com>
2022-07-05 00:58:17 +0530

    added proper links

--- server/services/render.js
+++ server/services/render.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
+    axios.get('https://dazzling-congaree-84171.herokuapp.com/api/users')
         .then(function (response) {
             res.render('index', { users: response.data });
         })
@@ -16,7 +16,7 @@ exports.add_user = (req, res) => {
 }
 
 exports.update_user = (req, res) => {
-    axios.get('http://localhost:3000/api/users', { params: {id: req.query.id }})
+    axios.get('https://dazzling-congaree-84171.herokuapp.com/api/users', { params: {id: req.query.id }})
         .then(function (userdata) {
             res.render('update_user', { user: userdata.data });
         })
