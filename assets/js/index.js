6208a3f90288d597b7f1c5879a539dc84f9ec036
Yatin <yatinrathod612@gmail.com>
2022-07-08 23:51:06 +0530

    Initial Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,7 @@ $("#update_user").submit(function (event) {
 
 
     var request = {
-        "url": `https://dazzling-congaree-84171.herokuapp.com/api/users/${data.id}`,
+        "url": `http://localhost:3000/api/users/${data.id}`,
         "method": "PUT",
         "data": data
     }
@@ -32,7 +32,7 @@ if (window.location.pathname == "/") {
         var id = $(this).attr("data-id")
 
         var request = {
-            "url": `https://dazzling-congaree-84171.herokuapp.com/api/users/${id}`,
+            "url": `http://localhost:3000/api/users/${id}`,
             "method": "DELETE"
         }
 
--- server/database/connection.js
+++ server/database/connection.js
@@ -1,42 +1,18 @@
 const mongoose = require('mongoose');
 
-// const connectDB = async ()=> {
-//     try {
-//         const con = await mongoose.connect(process.env.MONGO_URI, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
-
-//         console.log(`MongoDB Connected: ${con.connection.host}`);
-
-//     } catch (error) {
-//         console.log(error);
-//         process.exit(1);
-//     }
-// }
-
 const connectDB = async ()=> {
-    
-    try{
-        const con = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-        console.log(`MongoDB Connected`);
+    try {
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log(`MongoDB Connected: ${con.connection.host}`);
 
     } catch (error) {
         console.log(error);
         process.exit(1);
     }
 }
-
-
-
-// mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-
-// const connectDB = mongoose.connection;
-// connectDB.on('error', console.error.bind(console, 'Connection Error: '));
-
-// connectDB.once('open', function(){
-//     console.log("Database Connected");
-// });
-
 
 module.exports = connectDB;
