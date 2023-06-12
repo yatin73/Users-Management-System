057b12710c77dfeb4b22f9d8bb66054fe781b484
yatin73 <60352795+yatin73@users.noreply.github.com>
2023-06-12 16:20:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const dotenv = require('dotenv');
+require('dotenv').config()
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const path = require('path');
@@ -8,7 +8,6 @@ const connectDB = require('./server/database/connection');
 
 const app = express();
 
-dotenv.config({path: 'config.env'});
 const PORT = process.env.PORT || 3000;
 
 //log requests
